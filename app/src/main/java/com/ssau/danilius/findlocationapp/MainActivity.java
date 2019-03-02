da3ff254591fad1440c63c26a992444f1d4eae4b
Danila Malchikov <2regvk@mail.ru>
2019-03-03 00:34:15 +0400

    fix(AndroidFindLocationApp) Исправление части багов

--- app/src/main/java/com/ssau/danilius/findlocationapp/MainActivity.java
+++ app/src/main/java/com/ssau/danilius/findlocationapp/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     TextView xDistanceTextView;
     TextView yDistanceTextView;
     TextView zDistanceTextView;
+    TextView toOneMetres;
     Boolean startFlag = null;
     float dx = 0.0f;
     float vx = 0.0f;
@@ -40,6 +41,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         xDistanceTextView = (TextView) findViewById(R.id.xDistance);
         yDistanceTextView = (TextView) findViewById(R.id.yDistance);
         zDistanceTextView = (TextView) findViewById(R.id.zDistance);
+        toOneMetres = (TextView) findViewById(R.id.toOneMultiply);
 
         long curTime = System.currentTimeMillis();
 
@@ -58,12 +60,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                         startFlag = false;
                         btnStartFindLocation.setBackgroundColor(Color.RED);
                         GetData(data);
+                        data = CheckData(data);
                         data = SimpleFilter.FilterListSignal(data,(float)0.05); //(float) 5.23e-6
                         resultData = CalculateDistance.GetDistance(1, data);
                         GetData(data);
-                        xDistanceTextView.setText("Расстояние X:" + String.valueOf(resultData[0]/1e15));
-                        yDistanceTextView.setText("Расстояние Y:" + String.valueOf(resultData[1]/1e15));
-                        zDistanceTextView.setText("Расстояние Z:" + String.valueOf(resultData[2]/1e15));
+                        xDistanceTextView.setText("Расстояние X:" + String.valueOf(resultData[0]*0.0002264));///1e15
+                        yDistanceTextView.setText("Расстояние Y:" + String.valueOf(resultData[1]*0.0002264));
+                        zDistanceTextView.setText("Расстояние Z:" + String.valueOf(resultData[2]*0.0002264));
+                        toOneMetres.setText("До метра (без коэф.):"+ String.valueOf(1/resultData[0])  );
+                        sensorManager.unregisterListener((SensorEventListener) MainActivity.this);
                         data = new ArrayList<float[]>(){};
                     } else {
                         startFlag = true;
@@ -93,8 +98,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     }
 
-
-
     public void GetData(List<float[]> data) {
         String Resultx = "",Resulty = "",Resultz = "";
         for (float[] value: data) {
@@ -105,6 +108,32 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         Resultx += "end";
         Resulty += "end";
         Resultz += "end";
+    }
+
+    public List<float[]>  CheckData(List<float[]> data) {
+        Boolean check = false;
+        int countX=0,countY=0,countZ =0;
+        for (float[] value: data) {
+            if (data.get(0)[0] == value[0]){countX++;}
+            if (data.get(0)[1] == value[1]){countY++;}
+            if(data.get(0)[2] == value[2]){countZ++;}
+        }
+        if (countX == data.size()) {
+            for (float[] value : data) {
+                value[0] = 0;
+            }
+        }
+        if (countY == data.size()) {
+            for (float[] value : data) {
+                value[1] = 0;
+            }
+        }
+        if (countZ == data.size()) {
+            for (float[] value : data) {
+                value[2] = 0;
+            }
+        }
+        return data;
     }
 
 }
