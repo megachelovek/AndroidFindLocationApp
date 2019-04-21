ec970483879325084649cfcecf95b54733145854
Danila Malchikov <2regvk@mail.ru>
2019-04-22 00:03:23 +0400

    TODO commit

---
+++ app/src/main/java/com/ssau/danilius/findlocationapp/KalmanFilterAdapter.java
@@ -0,0 +1,13 @@
+package com.ssau.danilius.findlocationapp;
+
+import org.apache.commons.math3.filter.DefaultProcessModel;
+import org.apache.commons.math3.filter.ProcessModel;
+
+import java.util.List;
+
+public class KalmanFilterAdapter {
+    ProcessModel model = new DefaultProcessModel(,)//TODO
+    public static List<float[]> FilterListSignal(){
+
+    }
+}
